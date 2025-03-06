21b58a8f79804a48b355ce1f9ebc3e6ffce89005
bochora3000 <irakli.bochorishvili.2@gmail.com>
2025-03-06 16:01:46 +0100

    Implemented csv initiliaztion and adding entry

--- main.py
+++ main.py
@@ -5,13 +5,37 @@ from datetime import datetime
 
 class CSV:
     CSV_FILE = "finance_data.csv"
+    COLUMNS = ["date","amount","category","description"]
     
     @classmethod
     def initialize_csv(cls):
+        """
+        Ensures that the CSV file exists.
+        If the file doesn't exist, it creates one with the required column names.
+        """
         try:
             pd.read_csv(cls.CSV_FILE)
         except FileNotFoundError:
-            df = pd.DataFrame(columns=["date","amount","category","description"])
+            df = pd.DataFrame(columns=cls.COLUMNS)
             df.to_csv(cls.CSV_FILE, index=False)
+    
+    @classmethod
+    def add_entry(cls, date, amount, category, description):
+        """
+        Adds a new entry (row) to the CSV file.
+        """
+        new_entry = {
+            "date":date,
+            "amount":amount,
+            "category":category,
+            "description":description
+        }
+        
+        with open(cls.CSV_FILE, "a",newline="") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=cls.COLUMNS)
+            writer.writerow(new_entry)
+        
+        print("Entry added succesfully")
 
 CSV.initialize_csv()
+CSV.add_entry('2025-05-06',100,'Groceries','I bought carrots')
